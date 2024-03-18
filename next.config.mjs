eec345606859ad721cd6391be9b514bf9e17f670
Yakitori <89288375+Oni-giri@users.noreply.github.com>
2024-03-19 00:26:45 +0200

    add output dir

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import withSearch from './src/markdoc/search.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  output: 'export'
 }
 
 export default withSearch(
